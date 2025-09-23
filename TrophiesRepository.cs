0fc3c08e7d0d4863809062d7948eb243e124447a
Abdi22-7 <ah8808573@gmail.com>
2025-09-23 13:22:52 +0200

    CRUD funktioner tilføjet

--- TrophiesRepository.cs
+++ TrophiesRepository.cs
@@ -44,6 +44,42 @@
         {
             return _trophies.FirstOrDefault(t => t.Id == id); // returnerer id eller null
         }
+
+        public void Add(Trophy trophy)
+        {
+            if (trophy is null)
+                throw new ArgumentNullException(nameof(trophy), "Trophy må ikke være null.");
+            // Tjek om der allerede findes en trophy med samme Id
+            if (_trophies.Any(t => t.Id == trophy.Id))
+                throw new ArgumentException($"En trophy med Id {trophy.Id} findes allerede.", nameof(trophy));
+            _trophies.Add(CopyTrophy(trophy)); // Tilføj en kopi af trofæet
+        }
+        public bool Remove(int id)
+        {
+            var trophyToRemove = _trophies.FirstOrDefault(t => t.Id == id);
+            if (trophyToRemove != null)
+            {
+                _trophies.Remove(trophyToRemove);
+                return true; // Fjernelse lykkedes
+            }
+            return false; // Ingen trophy med det givne Id blev fundet
+        }
+
+        public Trophy? Update(int id, Trophy values)
+        {
+            // Find det eksisterende objekt med samme id
+            var existingTrophy = _trophies.FirstOrDefault(t => t.Id == id);
+
+            if (existingTrophy is null)
+                return null; // fandt ikke noget
+
+            // Opdaterer felterne fra values (Id ændres ikke)
+            existingTrophy.Competition = values.Competition;
+            existingTrophy.Year = values.Year;
+
+            return existingTrophy;
+        }
+
     }
 }
 
